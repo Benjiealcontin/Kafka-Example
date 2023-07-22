85513e554c5ff1213473adb26fc9dea9b64a64ae
Benjiealcontin <Benjiealcontin23@gmail.com>
2023-07-23 07:10:00 +0800

    Kafka basic with email notification

--- Consumer-Kafka/src/main/java/com/example/ConsumerKafka/Service/KafkaMessageListener.java
+++ Consumer-Kafka/src/main/java/com/example/ConsumerKafka/Service/KafkaMessageListener.java
@@ -1,8 +1,11 @@
 package com.example.ConsumerKafka.Service;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.Message;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
@@ -12,26 +15,47 @@ import org.springframework.messaging.handler.annotation.Header;
 @Service
 public class KafkaMessageListener {
 
+    @Autowired
+    private EmailSenderService emailSenderService;
+
     Logger log = LoggerFactory.getLogger(KafkaMessageListener.class);
 
     @KafkaListener(topics = "test", groupId = "jt-group-new")
-    public void consume1(String message, @Header("kafka_receivedMessageKey") String key) {
-        log.info("Consumer1 consumed the message with key=[{}] and value=[{}]", key, message);
+    public void consume1(ConsumerRecord<String, String> record) {
+        String key = record.key();
+        String message = record.value();
+        int partition = record.partition();
+        long offset = record.offset();
+
+        log.info("Consumer1 received the message with key=[{}] and value=[{}] from partition=[{}] with offset=[{}]",
+                key, message, partition, offset);
+        emailSenderService.sendSimpleEmail("alcontinebenezer07@gmail.com",key,message);
     }
 
     @KafkaListener(topics = "test", groupId = "jt-group-new")
-    public void consume2(String message, @Header("kafka_receivedMessageKey") String key) {
-        log.info("Consumer2 consumed the message with key=[{}] and value=[{}]", key, message);
+    public void consume2(ConsumerRecord<String, String> record) {
+        String key = record.key();
+        String message = record.value();
+        int partition = record.partition();
+        long offset = record.offset();
+
+        log.info("Consumer2 received the message with key=[{}] and value=[{}] from partition=[{}] with offset=[{}]",
+                key, message, partition, offset);
+        emailSenderService.sendSimpleEmail("alcontinebenezer07@gmail.com",key,message);
     }
 
 
     @KafkaListener(topics = "test", groupId = "jt-group-new")
-    public void consume3(String message, @Header("kafka_receivedMessageKey") String key) {
-        log.info("Consumer3 consumed the message with key=[{}] and value=[{}]", key, message);
-    }
+    public void consume3(ConsumerRecord<String, String> record) {
+        String key = record.key();
+        String message = record.value();
+        int partition = record.partition();
+        long offset = record.offset();
 
-//    @KafkaListener(topics = "javatechie-demo1",groupId = "jt-group-new")
-//    public void consume4(String message) {
-//        log.info("consumer4 consume the message {} ", message);
-//    }
+        log.info("Consumer3 received the message with key=[{}] and value=[{}] from partition=[{}] with offset=[{}]",
+                key, message, partition, offset);
+        emailSenderService.sendSimpleEmail("alcontinebenezer07@gmail.com","Notification",message);
+    }
 }
+
+
